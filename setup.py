c307944d8c81fd65e0b5e7e1f41235e76e15d173
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2021-12-11 18:07:41 +0100

    Bump version: 0.0.1 → 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author='Gabriel Montagné Láscaris-Comneno',
     author_email='gabriel@tibas.london',
     license='MIT',
-    version='0.0.1',
+    version='0.1.0',
     entry_points={
         'console_scripts': [
             'shonenbat = shonenbat.__main__:main'
