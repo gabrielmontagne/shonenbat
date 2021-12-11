acdfd9a8df1d543740ca56f6d1f0b29adccddb01
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2021-12-11 17:47:05 +0100

    add tokens and options args

--- shonenbat/__main__.py
+++ shonenbat/__main__.py
@@ -1,4 +1,6 @@
+from argparse import ArgumentParser, FileType
 from dotenv import load_dotenv
+import sys
 import openai
 import os
 
@@ -7,9 +9,25 @@ def main():
     """Run completion"""
 
     load_dotenv()
-    openai.organization = os.getenv("OPENAI_ORG_ID")
     openai.api_key = os.getenv("OPENAI_API_KEY")
-    print(openai.Engine.list())
+
+    parser = ArgumentParser()
+    parser.add_argument('prompt', nargs='?', type=FileType('r'), default=sys.stdin)
+    parser.add_argument('--max_tokens', '-mt', type=int, default=50)
+    parser.add_argument('--num_options', '-n', type=int, default=2)
+    args = parser.parse_args()
+    prompt = args.prompt.read().strip()
+
+    print('max tokens', args.max_tokens)
+
+    print(
+        openai.Completion.create(
+            engine='davinci',
+            prompt=prompt,
+            max_tokens=args.max_tokens,
+            n=args.num_options
+        )
+    )
 
 if __name__ == '__main__':
     main()
